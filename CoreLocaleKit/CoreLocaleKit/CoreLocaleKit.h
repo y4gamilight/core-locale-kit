f9b539f3b30a23eb0ea7db7748d8205b4a8781d8
Le Tan Thanh (Thomas) <ftt.thanh.letan@grabtaxi.com>
2024-12-24 09:15:13 +0700

    (FEAT) Setup project

---
+++ CoreLocaleKit/CoreLocaleKit/CoreLocaleKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreLocaleKit.h
+//  CoreLocaleKit
+//
+//  Created by Thanh Le Tan on 24/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreLocaleKit.
+FOUNDATION_EXPORT double CoreLocaleKitVersionNumber;
+
+//! Project version string for CoreLocaleKit.
+FOUNDATION_EXPORT const unsigned char CoreLocaleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreLocaleKit/PublicHeader.h>
+
+
---
+++ CoreLocaleKitExample/CoreLocaleKitExample/CoreLocaleKitExampleApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoreLocaleKitExampleApp.swift
+//  CoreLocaleKitExample
+//
+//  Created by Thanh Le Tan on 24/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoreLocaleKitExampleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
